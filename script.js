f1e5bfc9c0b77d411fd35c33f1d000508c3fb60e
claudia <claudia.matei@libero.it>
2023-11-30 21:05:35 +0100

    modifica 30.11

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 function cambiaColore(random) {
-    var caratteri = '0123456789ABCDEF';
-    var colore = '#';
-    for (var i = 0; i < 6; i++) {
-      colore += caratteri[Math.floor(Math.random() * 16)];
-    }
-    return colore;
+  var caratteri = '0123456789ABCDEF';
+  var colore = '#';
+  for (var i = 0; i < 6; i++) {
+    colore += caratteri[Math.floor(Math.random() * 16)];
   }
-  
-  function cambiaColoreParagrafi() {
-    var paragrafi = document.getElementsByClassName("paragrafo");
-    for (var i = 0; i < paragrafi.length; i++) {
-      paragrafi[i].style.color = cambiaColore();
-    }
+  return colore;
+}
+
+function cambiaColoreParagrafi() {
+  var paragrafi = document.getElementsByClassName("paragrafo");
+  for (var i = 0; i < paragrafi.length; i++) {
+    paragrafi[i].style.color = cambiaColore();
   }
-  
-  function rendeGrassetto() {
-    var paragrafi = document.getElementsByClassName("paragrafo");
-    for (var i = 0; i < paragrafi.length; i++) {
-      paragrafi[i].style.fontWeight = "bold";
-    }
+}
+
+function rendeGrassetto() {
+  var paragrafi = document.getElementsByClassName("paragrafo");
+  for (var i = 0; i < paragrafi.length; i++) {
+    paragrafi[i].style.fontWeight = "bold";
   }
-  
-  function nascondiParagrafi() {
-    var paragrafi = document.getElementsByClassName("paragrafo");
-    for (var i = 0; i < paragrafi.length; i++) {
-      paragrafi[i].style.display = "none";
-    }
+}
+
+function nascondiParagrafi() {
+  var paragrafi = document.getElementsByClassName("paragrafo");
+  for (var i = 0; i < paragrafi.length; i++) {
+    paragrafi[i].style.display = "none";
   }
+}
